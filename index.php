22ce6d46b958ce3e42ff501396d3675311306f24
camilars <camila.rs122@gmail.com>
2018-12-27 23:08:01 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Ad Verum</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/logo-adVerum.png" rel="icon">
+  <!-- <link href="img/apple-touch-icon.png" rel="apple-touch-icon"> -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700|Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#intro" class="scrollto">Ad Verum</a></h1>
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#intro">Home</a></li>
+          <li><a href="#about">Quem Somos</a></li>
+          <li><a href="#contact">Contatos</a></li>
+          <li><a href="#cursos">Cursos</a></li>
+          <li><a href=""><i class="fa fa-shopping-cart"></i>
+                    <span id="cart"></span></a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+  <!--==========================
+    Intro Section
+  ============================-->
+  <section id="intro">
+
+    <div class="intro-text">
+      <h2>QUAL O MELHOR CURSO PARA CONCURSOS?</h2>
+      <p>CONVERSE COM CANDIDATOS APROVADOS EM 2018 E PERGUNTE SOBRE A AD VERUM</p>
+      <p>Estude diferente, estude inteligente, estude Ad Verum!</p>
+      <a href="#about" class="btn-get-started scrollto">Saiba mais</a>
+    </div>
+
+  </section>
+
+  <main id="main">
+
+    <!--==========================
+      Quem Somos
+    ============================-->
+    <section id="about" class="section-bg">
+      <div class="container-fluid">
+        <div class="section-header">
+          <h3 class="section-title">Quem Somos</h3>
+          <span class="section-divider"></span>
+          <p class="section-description">
+            Colocar todos os novos recursos da era digital a serviço da aprovação do aluno.<br>
+
+          </p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 about-img wow fadeInLeft">
+            <img src="img/about-img.jpg" alt="">
+          </div>
+
+          <div class="col-lg-6 content wow fadeInRight">
+            <h2>A preparação para concursos públicos e exame da OAB mudou.</h2>
+            <h3>Em um ambiente cada vez mais competitivo, onde o tempo disponível para estudo é cada vez mais escasso, eficiência tornou-se requisito indispensável para aprovação.</h3>
+            <p>
+              A GARANTIA DE SATISFAÇÃO disponível para os cursos de Rodadas de Estudo Dirigido (1ª fase concursos), Rodadas de Simulados (2ª fase concursos), Rodadas para Prova Oral e em seus Programas de Coaching permitem que o aluno cancele a contratação, recebendo 100% do valor investido de volta. Só quem conta preparação efetiva pode oferecer essa opção.
+              <br><br>
+              INDEPENDENTEMENTE do momento da preparação do candidato, a Ad Verum está pronta para atender as suas necessidades, inclusive, através de Planos de Estudo personalizados, da disponibilização de Cursos em PDF (iPDF) ou da elaboração de Recursos Administrativos com vistas a majoração da nota de sua prova.
+              <br><br>
+              Seja qual o for o momento de sua preparação, seja qual for a sua necessidade, a Ad Verum está pronta para atendê-lo.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!--==========================
+      Cursos
+    ============================-->
+    <section id="cursos" class="section-bg">
+      <div class="container">
+        <div class="section-header">
+          <h3 class="section-title">Cursos</h3>
+          <span class="section-divider"></span>
+        </div>
+          <div class="row">
+            <div class="col-sm-4 my-4">
+              <div class="card">
+                <img class="card-img-top" src="img/cursos/curso1.jpeg" alt="">
+                <div class="card-body">
+                  <h4 class="card-title">Cuso Preparatório<br>Prova oral</h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                </div>
+                  <div class="row">
+                    <div class="col">
+                      <p class="btn btn-danger btn-block">R$ 32,90</p>
+                    </div>
+                    <div class="col">
+                      <button class="btn btn-success btn-block addCart" >Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+              <div class="col-sm-4 my-4">
+                <div class="card">
+                  <img class="card-img-top" src="img/cursos/curso2.jpeg" alt="">
+                  <div class="card-body">
+                    <h4 class="card-title">Cuso Preparatório<br>Prova oral</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p class="btn btn-danger btn-block">R$ 32,90</p>
+                    </div>
+                    <div class="col">
+                      <button class="btn btn-success btn-block addCart">Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-sm-4 my-4">
+                <div class="card">
+                  <img class="card-img-top" src="img/cursos/curso3.jpeg" alt="">
+                  <div class="card-body">
+                    <h4 class="card-title">Rodadas de<br>estudo dirigido</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p class="btn btn-danger btn-block">R$ 32,90</p>
+                    </div>
+                    <div class="col">
+                      <button class="btn btn-success btn-block" id="addCart">Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-sm-4 my-4">
+                <div class="card">
+                  <img class="card-img-top" src="img/cursos/curso4.jpeg" alt="">
+                  <div class="card-body">
+                    <h4 class="card-title">Rodadas<br>de simulados</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p class="btn btn-danger btn-block">R$ 32,90</p>
+                    </div>
+                    <div class="col">
+                      <button class="btn btn-success btn-block" id="addCart">Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+              <div class="col-sm-4 my-4">
+                <div class="card">
+                  <img class="card-img-top" src="img/cursos/curso5.jpeg" alt="">
+                  <div class="card-body">
+                    <h4 class="card-title">Rodadas<br>de simulados</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p class="btn btn-danger btn-block">R$ 32,90</p>
+                    </div>
+                    <div class="col">
+                      <button class="btn btn-success btn-block" id="addCart">Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+              <div class="col-sm-4 my-4">
+                <div class="card">
+                  <img class="card-img-top" src="img/cursos/curso6.jpeg" alt="">
+                  <div class="card-body">
+                    <h4 class="card-title">Ciclo<br>de simulados</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <p class="btn btn-danger btn-block">R$ 32,90</p>
+                    </div>
+                    <div class="col">
+                      <button class="btn btn-success btn-block" id="addCart">Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!--==========================
+      Contact Section
+    ============================-->
+    <section id="contact">
+      <div class="container">
+        <div class="row wow fadeInUp">
+
+          <div class="col-lg-4 col-md-4">
+            <div class="contact-about">
+              <h3>Ad Verum</h3>
+              <p>Estude inteligente, estude Ad Verum!</p>
+              <div class="social-links">
+                <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+                <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-4">
+            <div class="info">
+              <div>
+                <i class="ion-ios-location-outline"></i>
+                <p>A108 Adam Street<br>New York, NY 535022</p>
+              </div>
+
+              <div>
+                <i class="ion-ios-email-outline"></i>
+                <p>info@example.com</p>
+              </div>
+
+              <div>
+                <i class="ion-ios-telephone-outline"></i>
+                <p>+1 5589 55488 55s</p>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="col-lg-5 col-md-8">
+            <div class="form">
+              <div id="sendmessage">Your message has been sent. Thank you!</div>
+              <div id="errormessage"></div>
+              <form action="" method="post" role="form" class="contactForm">
+                <div class="form-row">
+                  <div class="form-group col-lg-6">
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                    <div class="validation"></div>
+                  </div>
+                  <div class="form-group col-lg-6">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                    <div class="validation"></div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                  <div class="validation"></div>
+                </div>
+                <div class="form-group">
+                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                  <div class="validation"></div>
+                </div>
+                <div class="text-center"><button type="submit" title="Send Message">Enviar mensagem</button></div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- #contact -->
+
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 text-lg-left text-center">
+          <div class="copyright">
+            &copy; Copyright <strong>Ad Verum</strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            Designed by Camila
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+  <script src="js/cart.js" defer ></script>
+  <script>
+    //     $('#addCart').on('click', function(){
+    //       //var cart = document.getElemntById('#cart').innerHTML;
+    //       //cart = parseInt(cart);
+    //       //cart.innerHTML = "0";
+    //       var count = 0;
+    //       var add = count + 1;
+    //       $('#cart').html(add);
+    // }); 
+  </script>
+</body>
+</html>
---
+++ js/cart.js
@@ -0,0 +1,106 @@
+// window.onload = function() {
+// 	// body...
+// var contador = document.querySelector('.cart');
+
+// document.querySelector('button').addEventListener('click', function(){
+//   var numero = parseInt(contador.textContent) + 1;
+//   contador.textContent = numero;
+// 	});
+// }
+
+
+  //------------ custom code
+
+
+  // var count_click = 0;
+
+//AÑADE UN CLICK AL EJECUTAR LA FUNCIÓN
+// function count_click_add() {
+//   var count_click = 0;
+//   count_click += 1;
+//   $("#count_click").text(count_click);
+// }
+
+// //MUESTRA CUANTOS CLICK LLEVAMOS
+// // $("#count_click").text(count_click);
+
+// //AÑADE A TODOS LOS BOTONES CON EL NAME count_click QUE AL SER PULSADOS EJECUTEN EL CONTADOR
+// $( document ).ready(function(){
+//   $("button[name='count_click']").click(function(){
+//      count_click_add();
+//   });
+// });
+
+
+    // var el = document.querySelectorAll('.addCart');
+    //   for(var i=0; i < el.length; i++){
+    //   el[i].addEventListener('click', function () {
+    // var contador = document.querySelector('.cart');
+    //     var numero = parseInt(contador.textContent) + 1;
+    //     contador.textContent = numero;
+    //     });
+    //   }
+
+    //   addEventListener('click', function(){
+    // var numero = parseInt(contador.textContent) + 1;
+    // contador.textContent = numero;
+    // });
+
+// const $ = document.querySelector.bind(document);
+// const botao = $('.addCart');
+// const painel = $('.cart');
+
+// // cria a propriedade contador dinamicamente
+// painel.contador = 0;
+
+// botao.addEventListener('click', () => {
+//     // a cada clique, incrementa painel.contador exibindo seu estado mais atual 
+//     painel.contador++;
+//     painel.textContent = painel.contador;
+// });
+
+// -----------------------------------------------
+
+    // document.addEventListener("DOMContentLoaded", function () {
+
+    //     var el = document.querySelectorAll('.addCart');
+    //      for(var i=0; i < el.length; i++){
+    //        el[i].addEventListener('click', function () {
+    //        var contador = document.querySelector('.cart');
+    //        var numero = parseInt(contador.textContent) + 1;
+    //        contador.textContent = numero;
+    //        });
+    //       }
+
+// -----------------------------------------
+
+//       var a = document.querySelector.bind(document);
+//       var botao = document.getElementsByClassName("addCart");
+//       var painel = document.getElementsByClassName("cart");
+
+//       painel.contador = 0;
+
+//       botao.addEventListener('click', () => {
+// //     // a cada clique, incrementa painel.contador exibindo seu estado mais atual 
+//       painel.contador++;
+//       painel.textContent = painel.contador;
+//          });
+    // });
+
+
+    //   var el = document.querySelectorAll('.addCart');
+    //   for(var i=0; i < el.length; i++){
+    //   el[i].addEventListener('click', function () {
+    // var contador = document.querySelector('.cart');
+    //     var numero = parseInt(contador.textContent) + 1;
+    //     contador.textContent = numero;
+    //     });
+    //   }
+//     window.onload = function() {
+//   // body...
+//   var contador = 0;
+//   document.getElementById('cartAdd').onclick = function(){
+//     contador ++;
+//     document.getElementById('cart').innerHTML = contador;
+//   }
+// }
